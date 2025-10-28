464c457b36ccdef0a008637e7ed4b62c09143f26
KyleBrook <kacavinhal@gmail.com>
2025-10-27 22:43:18 -0300

    [dyad] Configuração do Meta Pixel em todas as páginas - wrote 5 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import MetaPixelTracker from "./components/analytics/MetaPixelTracker.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
@@ -18,6 +19,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <MetaPixelTracker />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/promo" element={<Promo />} />
